5feffeaaf45dfcb921a5c30274d9781d705bb2d0
shamilmelis <akastramormoley1st@mail.ru>
2023-07-03 23:11:07 +0600

    i create a git repo for my site

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const menuClick = document.querySelector('.burger_button');
+menuClick.addEventListener('click', function() {
+    let cont = document.getElementById('row_header');
+    if (cont.className === 'row') {
+        cont.className = 'rowActive';
+    } else {
+        cont.className = 'row';
+    }
+})
+
